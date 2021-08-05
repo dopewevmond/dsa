73525155e0745ed671fe5de49b7a20c531e35733
Edmund Wevamwo <dopewevmond@gmail.com>
2021-08-05 19:38:31 +0000

    efficient algorithm for fibonacci

--- 1_fibonacci.py
+++ 1_fibonacci.py
@@ -1,7 +1,19 @@
 #define a function to compute fibonacci numbers
 
-def fibonacci(n):
+def fibonacci_naive(n):
     if n <= 1:
         return n
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci_naive(n-1) + fibonacci_naive(n-2)
+
+#the naive implementation is slow since it uses a recursive function each time that
+#n is greater than 1
+def fibonacci_efficient(n):
+    if n <= 1:
+        return n
+    fib_list=[0,1]
+    for _ in range(n-1):
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib_list[-1]
+
+print(fibonacci_efficient(10))
