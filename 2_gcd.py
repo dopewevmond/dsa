c9ca9b16287ca0d4df36dd123ff0e8e7474a1452
Edmund Wevamwo <dopewevmond@gmail.com>
2021-08-07 00:44:48 +0000

    checking runtime

--- 2_gcd.py
+++ 2_gcd.py
@@ -1,8 +1,11 @@
 #this program finds the greatest common divisor of two numbers
 
+import time
+start = time.time()
+
 def find_gcd_naive(a, b):
     gcd = 0
-    for i in range(1, a+b+1):
+    for i in range(1, max(a,b)):
         if a%i==0 and b%i==0:
             gcd = i
     return gcd
@@ -12,3 +15,9 @@ def find_gcd_efficient(a, b):
     if b == 0:
         return a
     return find_gcd_efficient(b, a%b)
+
+print(find_gcd_efficient(3918848,1653264))
+
+
+end = time.time()
+print(end-start)
