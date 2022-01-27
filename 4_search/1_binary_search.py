a315360aee4f825beff996970ffe8aed6845b370
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-27 23:50:16 +0000

    recursive and iterative binary search

--- 4_search/1_binary_search.py
+++ 4_search/1_binary_search.py
@@ -13,4 +13,7 @@ def bin_search(seq, low, high, key):
 
 
 a = [3, 5, 8, 10, 12, 15, 18, 20, 20, 50, 60]
-print(bin_search(a, 0, 10, 50))
+print(bin_search(a, 0, 10, 50)) #should print 9 since the key 50, has an index of 9
+
+b = [1, 2, 4, 5, 8, 15, 20]
+print(bin_search(b, 0, 6, 9)) #should print 4 since 9 is greater than 8 (which has index of 4) but less than 15 (which has index of 5)
